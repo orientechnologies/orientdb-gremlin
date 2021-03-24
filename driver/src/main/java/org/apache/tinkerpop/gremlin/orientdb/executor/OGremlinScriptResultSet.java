eca6cfff8d5f76684c311fd308e17ce0cffff6e0
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2021-03-24 12:48:36 +0100

    Fix timeout exception management on traversal iterators

--- driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/executor/OGremlinScriptResultSet.java
+++ driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/executor/OGremlinScriptResultSet.java
@@ -1,15 +1,20 @@
 package org.apache.tinkerpop.gremlin.orientdb.executor;
 
+import com.orientechnologies.common.concur.OTimeoutException;
+import com.orientechnologies.common.concur.lock.OInterruptedException;
 import com.orientechnologies.common.exception.OException;
 import com.orientechnologies.orient.core.command.script.transformer.OScriptTransformer;
 import com.orientechnologies.orient.core.exception.OCommandExecutionException;
+import com.orientechnologies.orient.core.exception.OStorageException;
 import com.orientechnologies.orient.core.sql.executor.OExecutionPlan;
 import com.orientechnologies.orient.core.sql.executor.OQueryMetrics;
 import com.orientechnologies.orient.core.sql.executor.OResult;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraph;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalInterruptedException;
 
+import java.nio.channels.ClosedChannelException;
 import java.util.Map;
 import java.util.Optional;
 
@@ -18,90 +23,105 @@ import java.util.Optional;
  */
 public class OGremlinScriptResultSet implements OResultSet, OQueryMetrics {
 
-    private final String textTraversal;
-    protected Traversal traversal;
-    private OScriptTransformer transformer;
-    private boolean closeGraph;
-    private boolean closing = false;
+  private final String textTraversal;
+  protected Traversal traversal;
+  private OScriptTransformer transformer;
+  private boolean closeGraph;
+  private boolean closing = false;
 
-    long startTime;
-    long totalExecutionTime = 0;
+  long startTime;
+  long totalExecutionTime = 0;
 
-    public OGremlinScriptResultSet(String iText,Traversal traversal, OScriptTransformer transformer) {
-        this(iText,traversal, transformer, false);
+  public OGremlinScriptResultSet(String iText, Traversal traversal, OScriptTransformer transformer) {
+    this(iText, traversal, transformer, false);
+  }
+
+  public OGremlinScriptResultSet(String iText, Traversal traversal, OScriptTransformer transformer, boolean closeGraph) {
+    this.textTraversal = iText;
+    this.traversal = traversal;
+    this.transformer = transformer;
+    this.closeGraph = closeGraph;
+    this.startTime = System.currentTimeMillis();
+  }
+
+  @Override
+  public boolean hasNext() {
+    try {
+      return traversal.hasNext();
+    } catch (TraversalInterruptedException e) {
+      throw new OInterruptedException("Timeout expired");
+    } catch (OStorageException se) {
+      if (se.getCause() instanceof TraversalInterruptedException ||
+              se.getCause() instanceof ClosedChannelException
+      ) {
+        throw new OInterruptedException("Timeout expired");
+      }
+      throw se;
     }
+  }
 
-    public OGremlinScriptResultSet(String iText,Traversal traversal, OScriptTransformer transformer, boolean closeGraph) {
-        this.textTraversal = iText;
-        this.traversal = traversal;
-        this.transformer = transformer;
-        this.closeGraph = closeGraph;
-        this.startTime = System.currentTimeMillis();
+  @Override
+  public OResult next() {
+
+    long begin = System.currentTimeMillis();
+
+    try {
+      Object next = traversal.next();
+      return transformer.toResult(next);
+    } finally {
+      totalExecutionTime += (System.currentTimeMillis() - begin);
     }
+  }
 
-    @Override
-    public boolean hasNext() {
-        return traversal.hasNext();
-    }
-
-    @Override
-    public OResult next() {
-
-        long begin = System.currentTimeMillis();
-
-        try {
-          Object next = traversal.next();
-          return transformer.toResult(next);
-        } finally {
-          totalExecutionTime += (System.currentTimeMillis() - begin);
-        }
-    }
-
-    @Override
-    public void close() {
-        try {
-            traversal.close();
-            if (closeGraph) {
-                traversal.asAdmin().getGraph().ifPresent(graph -> {
-                    try {
-                        OrientGraph g = (OrientGraph) graph;
-                        if (!closing) {
-                            closing = true;
-                            g.close();
-                        }
-                    } catch (Exception e) {
-                        throw OException.wrapException(new OCommandExecutionException("Error closing the Graph "), e);
-                    }
-                });
+  @Override
+  public void close() {
+    try {
+      traversal.close();
+      if (closeGraph) {
+        traversal.asAdmin().getGraph().ifPresent(graph -> {
+          try {
+            OrientGraph g = (OrientGraph) graph;
+            if (!closing) {
+              closing = true;
+              g.close();
             }
-        } catch (Exception e) {
-            throw OException.wrapException(new OCommandExecutionException("Error closing the gremlin Result Set"), e);
-        }
+          } catch (Exception e) {
+            throw OException.wrapException(new OCommandExecutionException("Error closing the Graph "), e);
+          }
+        });
+      }
+    } catch (Exception e) {
+      throw OException.wrapException(new OCommandExecutionException("Error closing the gremlin Result Set"), e);
     }
+  }
 
-    @Override
-    public Optional<OExecutionPlan> getExecutionPlan() {
-        return Optional.empty();
-    }
+  @Override
+  public Optional<OExecutionPlan> getExecutionPlan() {
+    return Optional.empty();
+  }
 
-    @Override
-    public Map<String, Long> getQueryStats() {
-        return null;
-    }
+  @Override
+  public Map<String, Long> getQueryStats() {
+    return null;
+  }
 
-  @Override public String getStatement() {
+  @Override
+  public String getStatement() {
     return textTraversal;
   }
 
-  @Override public long getStartTime() {
+  @Override
+  public long getStartTime() {
     return startTime;
   }
 
-  @Override public long getElapsedTimeMillis() {
+  @Override
+  public long getElapsedTimeMillis() {
     return totalExecutionTime;
   }
 
-  @Override public String getLanguage() {
+  @Override
+  public String getLanguage() {
     return "gremlin";
   }
 }
